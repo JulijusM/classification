11b4909dc59935ac86d816c74300465b0ee67b78
JulijusM <julijusmotiejunas@gmail.com>
2022-01-29 20:38:01 +0200

    first commit

---
+++ boston_marathon_classification.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import random
+import sklearn.linear_model as sklm
+import sklearn.metrics as skm
+
+
+print('hello world')
