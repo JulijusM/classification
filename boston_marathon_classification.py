9192b59dd2c57a802f41ec9fc031c01f22b14231
JulijusM <julijusmotiejunas@gmail.com>
2022-01-30 09:04:14 +0200

    add class Runner

--- boston_marathon_classification.py
+++ boston_marathon_classification.py
@@ -7,5 +7,39 @@ import sklearn.metrics as skm
 import classification_metrics
 
 
+class Runner(object):
+    def __init__(self, name, gender, age, time):
+        self._name = name
+        self._feature_vec = np.array([age, time])
+        self._label = gender
+    
+    def feature_dist(self, other):
+        return ((self._feature_vec-other._feature_vec)**2).sum()**0.5
+
+    def get_time(self):
+        return self._feature_vec[1]
+    
+    def get_age(self):
+        return self._feature_vec[0]
+    
+    def get_label(self):
+        return self._label
+    
+    def get_features(self):
+        return self._feature_vec
+
+    def __str__(self):
+        return (f'{self._name}: {self.get_age()}, ' +
+                f'{self.get_time()}, {self._label}')
+    
+def build_marathon_examples(file_name):
+    df = pd.read_csv(file_name)
+    examples = []
+    for index, row in df.iterrows():
+        a = Runner(row['Name'],row['Gender'],row['Age'],row['Time'])
+        examples.append(a)
+    return examples
+
+
 
 
